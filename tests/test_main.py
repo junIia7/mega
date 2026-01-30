3182e06621b5eb6ffaf39ff193007172b3a0d9ef
root <root@db35ad22c978>
2026-01-30 05:26:23 +0000

    Fix #8: файл main.py пустой
    Resolves #8

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,16 +2,19 @@ import unittest
 from main import main_function
 
 class TestMain(unittest.TestCase):
-    def test_main_function(self):
+    def test_addition(self):
         result = main_function(2, 3, '+')
         self.assertEqual(result, 5)
 
+    def test_subtraction(self):
         result = main_function(10, 5, '-')
         self.assertEqual(result, 5)
 
+    def test_multiplication(self):
         result = main_function(4, 3, '*')
         self.assertEqual(result, 12)
 
+    def test_division(self):
         result = main_function(10, 2, '/')
         self.assertEqual(result, 5)
 
